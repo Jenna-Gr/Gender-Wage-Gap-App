6c52177b29ca534f0a34e02c0433347d0278079f
Jenna Groth <jenna.groth18@alumni.colostate.edu>
2021-05-18 19:07:35 -0600

    "Optimized filters, ready to add to db"

--- index.js
+++ index.js
@@ -1,57 +1,85 @@
 const puppeteer = require('puppeteer');
 
-async function run() {
+async function run(company, gender) {
   const browser = await puppeteer.launch({
     headless: false
   });
   const page = await browser.newPage();
+  // page.on('console', consoleObj => console.log(consoleObj.text()));
 
-  await page.goto('https://www.levels.fyi/company/VMware/salaries/Software-Engineer/');
+  await page.goto(`https://www.levels.fyi/company/${company}/salaries/Software-Engineer/`);
 
   // dom element selectors
   const BUTTON_SELECTOR = 'body > div.container.company-info-full-container > div > div.col-12.col-lg-10.company-header-col.about-company-header > div > div.compensation-table-levelbase.mb-4 > div.row.salaries-header > div.smallerSidePadding-md.col-7 > div.form-group.search-bar-container > div > span > button';
-  const FILTER_FEMALE_SELECTOR = '#genderFemaleRadioOption';
+  const FILTER_GENDER_SELECTOR = gender === 'female' ? '#genderFemaleRadioOption' : (gender === 'male' ? '#genderMaleRadioOption' : '#genderOtherRadioOption');
+  // const FILTER_female_SELECTOR = '#genderFemaleRadioOption';
+  // const FILTER_male_SELECTOR = '#genderMaleRadioOption';
+  // const FILTER_other_SELECTOR = '#genderOtherRadioOption';
+  const NEXT_PAGE_SELECTOR = 'body > div.container.company-info-full-container > div > div.col-12.col-lg-10.company-header-col.about-company-header > div > div.compensation-table-levelbase.mb-4 > div.spreadsheet > div > div > div.bootstrap-table.bootstrap4 > div.fixed-table-pagination > div.float-right.pagination > ul > li.page-item.page-next';
 
   await page.click(BUTTON_SELECTOR);
-  await page.click(FILTER_FEMALE_SELECTOR);
+  await page.click(FILTER_GENDER_SELECTOR);
   await page.click(BUTTON_SELECTOR);
-  await page.waitFor(6000);
+  await page.waitForTimeout(2000);
 
-  // const LIST_COMPANY_SELECTOR = '#compTable > tbody > tr:nth-child(1) > td.vcenter.text-left.otherWidth > span';
   const LIST_COMPANY_SELECTOR = '#compTable > tbody > tr:nth-child(INDEX) > td.vcenter.text-left.otherWidth > span';
-  // const LIST_LOCATION_SELECTOR = '#compTable > tbody > tr:nth-child(1) > td.vcenter.text-left.otherWidth > p > span';
   const LIST_LOCATION_SELECTOR = '#compTable > tbody > tr:nth-child(INDEX) > td.vcenter.text-left.otherWidth > p > span';
-  // const YOE_SELECTOR = '#compTable > tbody > tr:nth-child(1) > td.text-left.otherWidth.d-none.d-md-table-cell > span';
   const LIST_YOE_SELECTOR = '#compTable > tbody > tr:nth-child(INDEX) > td.text-left.otherWidth.d-none.d-md-table-cell > span';
-  // const COMPENSATION_SELECTOR = '#compTable > tbody > tr:nth-child(1) > td.text-right.otherWidth > div > div > span';
   const LIST_COMPENSATION_SELECTOR = '#compTable > tbody > tr:nth-child(INDEX) > td.text-right.otherWidth > div > div > span';
 
-  // const LENGTH_SELECTOR_CLASS = 'user-list-item';
+  const numUsers = await getNumUsers(page);
+  const numLeftUsers = numUsers % 10;
+  const numPages = Math.ceil(numUsers / 10);
 
-  // const numPages = await getNumPages(page);
 
-  // console.log('Numpages: ', numPages);
+  for (let h = 1; h <= numPages -1; h++) {
+    console.log(`Page ${h} of ${numPages}`);
+    for (let i = 1; i <= 10; i++) {
+      // change the index to the next child
+      let locationSelector = LIST_LOCATION_SELECTOR.replace("INDEX", i);
+      let yoeSelector = LIST_YOE_SELECTOR.replace("INDEX", i);
+      let compensationSelector = LIST_COMPENSATION_SELECTOR.replace("INDEX", i);
 
-  for (let i = 1; i <= 10; i++) {
+      let location = await page.evaluate((sel) => {
+        let element = document.querySelector(sel).innerHTML;
+        let cutBefore = element.indexOf('|') -1;
+        return element.slice(0, cutBefore);
+      }, locationSelector);
+
+      let yoe = await page.evaluate((sel) => {
+        let element = document.querySelector(sel).innerHTML;
+        let cutAfter = element.indexOf('/') + 2;
+        return element.slice(cutAfter);
+      }, yoeSelector);
+
+      let compensation = await page.evaluate((sel) => {
+        let element = document.querySelector(sel);
+        return element.innerText;
+      }, compensationSelector);
+
+      console.log(`Company: ${company}, Gender: ${gender}, Location: ${location}, Years of Experience: ${yoe}, Compensation: ${compensation}`);
+
+    }
+    await page.click(NEXT_PAGE_SELECTOR);
+    await page.waitForTimeout(2000);
+  }
+  console.log(`Page ${numPages} of ${numPages}`);
+  for (let i = 1; i <= numLeftUsers; i++) {
     // change the index to the next child
-    let companySelector = LIST_COMPANY_SELECTOR.replace("INDEX", i);
     let locationSelector = LIST_LOCATION_SELECTOR.replace("INDEX", i);
     let yoeSelector = LIST_YOE_SELECTOR.replace("INDEX", i);
     let compensationSelector = LIST_COMPENSATION_SELECTOR.replace("INDEX", i);
 
-    let company = await page.evaluate((sel) => {
-      let element = document.querySelector(sel);
-      return element.innerHTML;
-    }, companySelector);
-
     let location = await page.evaluate((sel) => {
-      let element = document.querySelector(sel);
-      return element.innerHTML;
+      let element = document.querySelector(sel).innerHTML;
+      let cutBefore = element.indexOf('|') -1;
+      return element.slice(0, cutBefore);
     }, locationSelector);
 
     let yoe = await page.evaluate((sel) => {
-      let element = document.querySelector(sel);
-      return element.innerHTML;
+      let element = document.querySelector(sel).innerHTML;
+      let cutAfter = element.indexOf('/') + 2;
+      return element.slice(cutAfter);
     }, yoeSelector);
 
     let compensation = await page.evaluate((sel) => {
@@ -59,88 +87,32 @@ async function run() {
       return element.innerText;
     }, compensationSelector);
 
-    console.log(`Company: ${company}, Location: ${location}, Years of Experience: ${yoe}, Compensation: ${compensation}`);
-
-    // TODO save this user
+    console.log(`Company: ${company}, Gender: ${gender}, Location: ${location}, Years of Experience: ${yoe}, Compensation: ${compensation}`);
   }
 
-  // for (let h = 1; h <= numPages; h++) {
-  //   let pageUrl = searchUrl + '&p=' + h;
-  //   await page.goto(pageUrl);
-
-  //   let listLength = await page.evaluate((sel) => {
-  //     return document.getElementsByClassName(sel).length;
-  //   }, LENGTH_SELECTOR_CLASS);
-
-  //   for (let i = 0; i <= listLength; i++) {
-  //     // change the index to the next child
-  //     let companySelector = LIST_COMPANY_SELECTOR.replace("INDEX", i);
-  //     let locationSelector = LIST_LOCATION_SELECTOR.replace("INDEX", i);
-  //     let yoeSelector = LIST_YOE_SELECTOR.replace("INDEX", i);
-  //     let compensationSelector = LIST_COMPENSATION_SELECTOR.replace("INDEX", i);
-
-  //     let  = await page.evaluate((sel) => {
-  //       return document.querySelector(sel).getAttribute('href').replace('/', '');
-  //     }, usernameSelector);
-
-  //     let email = await page.evaluate((sel) => {
-  //       let element = document.querySelector(sel);
-  //       return element ? element.innerHTML : null;
-  //     }, emailSelector);
-
-  //     // not all users have emails visible
-  //     if (!email)
-  //       continue;
-
-  //     console.log(username, ' -> ', email);
-
-  //     // upsertUser({
-  //     //   username: username,
-  //     //   email: email,
-  //     //   dateCrawled: new Date()
-  //     // });
-  //   }
-  // }
-
-  // browser.close();
+  browser.close();
 }
 
-async function getNumPages(page) {
-  const NUM_USER_SELECTOR = '#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > div.d-flex.flex-column.flex-md-row.flex-justify-between.border-bottom.pb-3.position-relative > h3';
-  let inner = await page.evaluate((sel) => {
-    let html = document.querySelector(sel).innerHTML;
+async function getNumUsers(page) {
+  let inner = await page.evaluate(() => {
+    let html = document.getElementsByClassName("pagination-info")[0].innerText;
 
-    // format is: "69,803 users"
-    return html.replace(',', '').replace('users', '').trim();
-  }, NUM_USER_SELECTOR);
+    // format is: "Showing 1 to 10 of 34 rows"
+    return html.slice(19).replace(' rows', '').trim();
+  });
 
-  const numUsers = parseInt(inner);
+  const numRows = parseInt(inner);
 
-  console.log('numUsers: ', numUsers);
-
-  /**
-   * GitHub shows 10 resuls per page, so
-   */
-  return Math.ceil(numUsers / 10);
+  console.log('\n\nnumRows:', numRows);
+  return numRows;
 }
 
-// function upsertUser(userObj) {
-//   const DB_URL = 'mongodb://localhost/thal';
+const companies = ['Google', 'Amazon', 'Facebook', 'Apple', 'Microsoft', 'VMware', 'IBM', 'Tesla', 'Spotify', 'Adobe'];
 
-//   if (mongoose.connection.readyState == 0) {
-//     mongoose.connect(DB_URL);
+// const generateDataFemale = (companies) => {
+//   for (var j = 0; j < companies.length; j++) {
+
 //   }
+// };
 
-//   // if this email exists, update the entry, don't insert
-//   const conditions = { email: userObj.email };
-//   const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-
-//   User.findOneAndUpdate(conditions, userObj, options, (err, result) => {
-//     if (err) {
-//       throw err;
-//     }
-//   });
-// }
-
-
-run();
+run(companies[5], 'female');
